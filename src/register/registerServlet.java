a1f6f9773b0425b82131f281d58aea3258ec890c
赵凯旋 <48469051+zkx1224@users.noreply.github.com>
2019-07-11 15:28:29 +0800

    Update registerServlet.java

--- src/register/registerServlet.java
+++ src/register/registerServlet.java
@@ -6,52 +6,42 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class registerServlet
- */
 @WebServlet("/regis")
-public class registerServlet extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public registerServlet() {
-        super();
-        // TODO Auto-generated constructor stub
+public class RegisServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        doPost(req,resp);
     }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		req.setCharacterEncoding("UTF-8");
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.setCharacterEncoding("UTF-8");
         String userName = req.getParameter("userName");
         String passWord = req.getParameter("passWord");
         String sex = req.getParameter("sex");
         String email = req.getParameter("email");
-        Data data = req.getParameter("data");
-        UserService userService =new UserServiceImpl();
-        User user = new User(userName,passWord,sex,email,data);
-        boolean userNameIsExit = userService.checkUserNameIsExit("userName");
-        if(userNameIsExit){
-            userService.addUser(user);
-            req.setAttribute("succ","success");
-            System.out.println("添加成功！");
-        }else{
-            req.setAttribute("succ","error");
-            System.out.println("用户名已存在！");
+        String date = req.getParameter("date");
+        System.out.println(date);
+        try {
+            Date date1 = new SimpleDateFormat("yyyy-MM-dd").parse(date);
+            UserService userService = new UserServiceImpl();
+            User user = new User(userName, sex, passWord, email,date1);
+            boolean userNameIsExit = false;
+            userNameIsExit = userService.checkUserNameIsExit("userName");
+            if (userNameIsExit) {
+                userService.addUser(user);
+                req.setAttribute("succ", "success");
+                System.out.println("添加成功！");
+            } else {
+                req.setAttribute("succ", "error");
+                System.out.println("用户名已存在！");
+            }
+        } catch (ParseException e) {
+            e.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
         }
-        resp.sendRedirect("regis.jsp");
-
-	}
-
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
-	}
-
+        resp.sendRedirect("/jsp/regis.jsp");
+    }
 }
+
