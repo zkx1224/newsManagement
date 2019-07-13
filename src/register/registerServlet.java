f73fa490d79b78ca2e575db9a5f797c6a48d95a1
赵凯旋 <48469051+zkx1224@users.noreply.github.com>
2019-07-13 17:28:23 +0800

    Update registerServlet.java

--- src/register/registerServlet.java
+++ src/register/registerServlet.java
@@ -1,47 +1,40 @@
-package register;
-
+package service.register;
 import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.*;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import entity.User;
+import service.UserService;
+import service.UserServiceImpl;
 
-@WebServlet("/regis")
-public class RegisServlet extends HttpServlet {
-    @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        doPost(req,resp);
-    }
+public class registerService extends HttpServlet {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
-    @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+	}
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         req.setCharacterEncoding("UTF-8");
         String userName = req.getParameter("userName");
         String passWord = req.getParameter("passWord");
         String sex = req.getParameter("sex");
         String email = req.getParameter("email");
-        String date = req.getParameter("date");
-        System.out.println(date);
+        String  date = req.getParameter("date");
         try {
             Date date1 = new SimpleDateFormat("yyyy-MM-dd").parse(date);
             UserService userService = new UserServiceImpl();
-            User user = new User(userName, sex, passWord, email,date1);
-            boolean userNameIsExit = false;
-            userNameIsExit = userService.checkUserNameIsExit("userName");
-            if (userNameIsExit) {
-                userService.addUser(user);
-                req.setAttribute("succ", "success");
-                System.out.println("添加成功！");
-            } else {
-                req.setAttribute("succ", "error");
-                System.out.println("用户名已存在！");
+            User user = new User(userName, sex, passWord, email, date1);
+            System.out.println("userName:"+userName);
+            boolean userNameIsExit = userService.checkUserNameIsExit(userName);
+            if(userNameIsExit!=true){
+              userService.addUser(user);
             }
-        } catch (ParseException e) {
-            e.printStackTrace();
-        } catch (SQLException e) {
+            resp.sendRedirect("regis.jsp");
+        } catch (Exception e) {
             e.printStackTrace();
         }
-        resp.sendRedirect("/jsp/regis.jsp");
-    }
-}
 
+    }
+
+}
